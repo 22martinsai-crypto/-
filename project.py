2b7f691ad2e575e7ff17bcf98282f7e41cbd3612
22martinsai-crypto <22martinsai@hazelwick.org>
2026-01-09 14:53:43 +0000

    9/1/2026

---
+++ project.py
@@ -0,0 +1,9 @@
+name = ""
+chos1 = ""
+chos1=input("Would you like to sign-up and create a new account?")
+if chos1 == "Yes" or chos1 == "yes":
+    name = input("Enter your name: ")
+elif chos1 == "No" or chos1 == "no":
+    print("Leave then")
+
+    
