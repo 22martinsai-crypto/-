ffbc76ebb03738beb87e523a9c3c669af93be05a
22martinsai-crypto <22martinsai@hazelwick.org>
2026-01-21 15:20:42 +0000

    Refactor user input prompts and messages

--- project.py
+++ project.py
@@ -1,9 +1,7 @@
-name = ""
-chos1 = ""
 chos1=input("Would you like to sign-up and create a new account?")
 if chos1 == "Yes" or chos1 == "yes":
-    name = input("Enter your name: ")
+    name = input("Enter your name:")
+    pasw = input("Enter your password:")
+    chos2 = input("Welcome to a cheap copy of Connect 4 on python code which you're playing because you're too broke to afford the game! Would you like to play against AI or with a friend (AI/2 player)?")
 elif chos1 == "No" or chos1 == "no":
-    print("Leave then")
-
-    
+    print("Too bad, logging-in is not an option. Goodbye")
